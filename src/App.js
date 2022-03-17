27c14fd93f1803f2725d7397ef717cadc77372a3
sinha462 <60381958+sinha462@users.noreply.github.com>
2022-03-17 09:28:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div class="container">
+        <span class="e19_720">About us</span>
+        <div class="e19_721">
+          Welcome to GASTOS, your number one solution of your daily payments.
+          We're dedicated to providing you with the best of the services, with a
+          focus on dependability. customer service, and your benefits.
+          <br />
+          <br />
+          GASTOS is FinTech Startup Based in Chandigarh aimed to uplift the
+          local and small businesses. Gastos is an Application based startup
+          which helps user to save their money while doing payments at their
+          local shops and businesses also helping them to manage all of their
+          expenses at one place.
+          <br />
+          Gastos has a potential of becoming a Billion Dollar Industry in coming
+          years and has a potential user base of more than 200 Million to
+          target. Gastos is the only startup in India which is bringing UPI
+          Digital cards for the users which helps them to save their money.
+          <br />
+          <br />
+          During this Covid-19 situation time and between the hard races of
+          online delivery, the local and small scaled businesses are mostly
+          affected. Gastos is the startup which is promoting these small
+          businesses to grow and become a brand before the user. Gastos is
+          solving multiple problems for users like fake discounts, lack of
+          saving,
+          <br />
+          unmanaged expenses.
+          <br />
+          <br />
+          We're working to turn our passion for the services into a booming
+          Market Network. We hope you enjoy our services as much as we enjoy
+          offering them to you.
+          <br />
+          <br />
+          <br />
+          Sincerely,
+          <br />
+          Sahgal Kumar
+          <br />
+          (Founder)
+          <br />
+          <br />
+          here are some following ways through which you will be connected with
+          us.
+          <br />
+          <br />
+          • Instagram : gastos.in
+          <br />
+          • Facebook : GASTOS DEALZ
+          <br />
+          • Twitter : gastos_in
+          <br />
+          For any queries you can contact us as help@gastos.in <br />
+          <br />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
